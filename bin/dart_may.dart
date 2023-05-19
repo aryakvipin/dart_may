d029a4a957f044e13d4d981558218f4a08f27e9d
aryakvipin <arya28985@gmail.com>
2023-05-19 15:40:34 +0530

    Initial commit

---
+++ bin/dart_may.dart
@@ -0,0 +1,5 @@
+import 'package:dart_may/dart_may.dart' as dart_may;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_may.calculate()}!');
+}
---
+++ bin/sample.dart
@@ -0,0 +1,5 @@
+// retun type  fucntion anme(){}
+ void  main(){
+ print(2255);
+ print("flutter");
+ }
